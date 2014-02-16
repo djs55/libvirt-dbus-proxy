f684a95bd01e7e63158603bbffd86adf2725e589
David Scott <dave.scott@eu.citrix.com>
2014-02-16 17:00:30 +0000

    Fork a subprocess, marshal events over a pipe
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- src/domain_events.ml
+++ src/domain_events.ml
@@ -12,7 +12,8 @@ module D = Libvirt.Domain
 module E = Libvirt.Event
 module N = Libvirt.Network
 
-type any =
+module Any = struct
+  type t =
   | Lifecycle of E.Lifecycle.t
   | Reboot of E.Reboot.t
   | RtcChange of E.Rtc_change.t
@@ -28,6 +29,25 @@ type any =
   | PMSuspend of E.PM_suspend.t
   | BalloonChange of E.Balloon_change.t
   | PMSuspendDisk of E.PM_suspend_disk.t
+
+  let to_string = function
+  | Lifecycle x -> E.Lifecycle.to_string x
+  | Reboot x -> E.Reboot.to_string x
+  | RtcChange x -> E.Rtc_change.to_string x
+  | Watchdog x -> E.Watchdog.to_string x
+  | IOError x -> E.Io_error.to_string x
+  | Graphics x -> E.Graphics.to_string x
+  | IOErrorReason x -> E.Io_error.to_string x
+  | ControlError x -> E.Control_error.to_string x
+  | BlockJob x -> E.Block_job.to_string x
+  | DiskChange x -> E.Disk_change.to_string x
+  | TrayChange x -> E.Tray_change.to_string x
+  | PMWakeUp x -> E.PM_wakeup.to_string x
+  | PMSuspend x -> E.PM_suspend.to_string x
+  | BalloonChange x -> E.Balloon_change.to_string x
+  | PMSuspendDisk x -> E.PM_suspend_disk.to_string x
+
+end
 
 let string_of_state = function
   | D.InfoNoState -> "no state"
@@ -90,6 +110,8 @@ let with_all_events name f =
             (* Check for GC errors: *)
             Gc.compact ()
         ) in
+
+    let open Any in
 
     let (_: E.callback_id) = E.register_any conn (E.Lifecycle (fun dom e ->
         printd dom "Lifecycle %s" (E.Lifecycle.to_string e);
@@ -159,5 +181,53 @@ let with_all_events name f =
     Libvirt.Virterror err ->
       eprintf "error: %s\n" (Libvirt.Virterror.to_string err)
 
-let () =
-  with_all_events None (fun _ _ -> ())
+let event_buffer_length = 1024
+let length_buffer_length = 2
+
+let open_events () =
+  let reader, writer = Unix.pipe () in
+  let child = Unix.fork () in
+  if child = 0 then begin
+    Unix.close reader;
+    let buf = String.make event_buffer_length '\000' in
+    let len_buf = String.make length_buffer_length '\000' in
+    with_all_events None (fun d e ->
+      let len = Marshal.to_buffer buf 0 event_buffer_length (d, e) [] in
+      len_buf.[0] <- char_of_int (len / 256);
+      len_buf.[1] <- char_of_int (len mod 256);
+      let n = Unix.write writer len_buf 0 length_buffer_length in
+      if n <> length_buffer_length then failwith "event pipe: short write";
+      let n = Unix.write writer buf 0 len in
+      if n <> len then failwith "event pipe: short write"
+    );
+    exit 1
+  end;
+  Unix.close writer;
+  reader
+
+open Lwt
+
+let complete op fd buf ofs len =
+  let rec loop acc fd buf ofs len =
+    op fd buf ofs len >>= fun n ->
+    let len' = len - n in
+    let acc' = acc + n in
+    if len' = 0 || n = 0
+    then return acc'
+    else loop acc' fd buf (ofs + n) len' in
+  loop 0 fd buf ofs len >>= fun n ->
+  if n = 0 && len <> 0
+  then fail End_of_file
+  else return ()
+
+let read_events reader =
+  let fd = Lwt_unix.of_unix_file_descr reader in
+  let buf = String.make event_buffer_length '\000' in
+  let len_buf = String.make length_buffer_length '\000' in
+  let rec loop () =
+    complete Lwt_unix.read fd len_buf 0 length_buffer_length >>= fun () ->
+    let len = (int_of_char len_buf.[0]) * 256 + (int_of_char len_buf.[1]) in
+    complete Lwt_unix.read fd buf 0 len >>= fun () ->
+    let d, e = Marshal.from_string buf 0 in
+    loop () in
+  loop ()
